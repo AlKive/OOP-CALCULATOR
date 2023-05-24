c985bde5e08b7b2ba256cb3d0dd080d08e262d5a
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-24 12:43:52 +0800

    Created an input function for number and arithmetic values

--- Alexa's Calculator/CalculatorClass.py
+++ Alexa's Calculator/CalculatorClass.py
@@ -1,7 +1,19 @@
 #PSEUDOCODE
-
 #create Calculator Class
+class Calculator:
     #create functions;
-        #get number inputs and arithmetic operation from user
+    #get number inputs and arithmetic operation from user
+    def user_inputs(self):
+        while True:
+            try:
+                #get number inputs
+                self.first_num =float(input("Enter your FIRST NUMBER:  " ))
+                self.second_num =float(input("Enter your SECOND NUMBER:  "))
+                #get arithmetic operation
+                self.operation = str(input("Choose an operation: \n If add, enter '+',\nIf subtract,enter '-'\n\tIf multiply,enter '*'\n\tIf divide,enter '/' "))
+                break
+            except:
+                print("INVALID INPUT!!!\nPlease enter a valid values")
+                continue
         #calculate the inputs
         #display the calculated results
