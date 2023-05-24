9b09d2c715b9f633cc74adab1ef2c700d4767c09
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-24 12:59:29 +0800

    Ceated a calculate function

--- Alexa's Calculator/CalculatorClass.py
+++ Alexa's Calculator/CalculatorClass.py
@@ -7,13 +7,39 @@ class Calculator:
         while True:
             try:
                 #get number inputs
-                self.first_num =float(input("Enter your FIRST NUMBER:  " ))
-                self.second_num =float(input("Enter your SECOND NUMBER:  "))
+                self.first_number =float(input("Enter your FIRST NUMBER:  " ))
+                self.second_number =float(input("Enter your SECOND NUMBER:  "))
                 #get arithmetic operation
                 self.operation = str(input("Choose an operation: \n If add, enter '+',\nIf subtract,enter '-'\n\tIf multiply,enter '*'\n\tIf divide,enter '/' "))
                 break
             except:
                 print("INVALID INPUT!!!\nPlease enter a valid values")
                 continue
-        #calculate the inputs
+            
+    #calculate the inputs
+    def Calculate(self) -> None:
+    # If one of the arguments is missing, throw an error
+        if (self.first_number and self.second_number and self.operation) == False:
+            raise NameError("You're missing an argument/s, check your inputs!")
+        if (self.first_number and self.second_number and self.operation) == True:
+            try:
+                self.first_number = float(self.first_number)
+                self.second_number = float(self.second_number)
+                # choosing peferred operation
+                match self.operation:
+                        case '+':
+                            self.result = self.first_number + self.second_number
+                        case '-':
+                            self.result = self.first_number - self.second_number
+                        case '*':
+                            self.result = self.first_number * self.second_number
+                        case '/':
+                            try:
+                                self.result = self.first_number / self.second_number
+                            except ZeroDivisionError:
+                                raise ZeroDivisionError("[ERROR] Division by Zero is not allowed!")
+                        case _:
+                            raise ValueError("UNSUPPORTED OPERATION")
+            except ValueError:
+                raise ValueError("NUMBERS ONLY")   
         #display the calculated results
