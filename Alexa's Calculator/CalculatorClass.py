2804151c3837eb10cd3a1f0e763a759f7fe496f8
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-05-24 12:17:45 +0800

    Created a PSEUDOCODE for CalculatorClass file

---
+++ Alexa's Calculator/CalculatorClass.py
@@ -0,0 +1,7 @@
+#PSEUDOCODE
+
+#create Calculator Class
+    #create functions;
+        #get number inputs and arithmetic operation from user
+        #calculate the inputs
+        #display the calculated results
